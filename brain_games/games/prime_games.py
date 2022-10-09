40011715df5a0e5189ed359f1c0dc9163455f27f
марк проскурин <proskurina-1956@mail.ru>
2022-10-09 09:05:09 +0300

    remove some more odor

--- brain_games/games/prime_games.py
+++ brain_games/games/prime_games.py
@@ -1,23 +1,25 @@
 from prompt import string
 from random import randint
 
+def return_result(divisors):
+    if len(divisors) == 2:
+        return 'yes'
+    else:
+        return 'no'
 
 def search_right_answer(number):
     if number == 1:
         return 'no'
     else:
-        separators = []
-        separator = number
-        while separator != 0:
-            if number % separator == 0:
-                separators.append(separator)
-                separator -= 1
+        divisors = []
+        divisor = number
+        while divisor != 0:
+            if number % divisor == 0:
+                divisors.append(divisor)
+                divisor -= 1
             else:
-                separator -= 1
-        if len(separators) == 2:
-            return 'yes'
-        else:
-            return 'no'
+                divisor -= 1
+        return return_result(divisors)
 
 
 def prime():
