6107dff10f34b97e4a8fe6250c07aa7708112893
марк проскурин <proskurina-1956@mail.ru>
2022-10-09 20:23:16 +0300

    fix bugs

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,15 +7,11 @@ answer = 'Find the greatest common divisor of given numbers.'
 def run_game():
     first_int = randint(1, 10)
     second_int = randint(1, 10)
-
     print(f'Question: {first_int} {second_int}')
-
     right_answer = min([first_int, second_int])
-
     while right_answer != 0:
         if first_int % right_answer == 0 and second_int % right_answer == 0:
             return right_answer
         else:
             right_answer -= 1
-    else:
-        return right_answer
+    return right_answer
--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,17 +6,14 @@ answer = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def run_game():
     number = randint(1, 25)
-
     print(f'Question: {number}')
-
     if number == 1:
         return 'no'
+    divisors = 0
+    for i in range(2, number // 2+1):
+        if (number % i == 0):
+            divisors = divisors + 1
+    if divisors == 0:
+        return 'yes'
     else:
-        divisors = 0
-        for i in range(2, number // 2+1):
-            if (number % i == 0):
-                divisors = divisors + 1
-        if divisors == 0:
-            return 'yes'
-        else:
-            return 'no'
+        return 'no'
