2ee568bce928ee7daa088e9f92944df8ed7f2e50
марк проскурин <proskurina-1956@mail.ru>
2022-10-13 13:46:38 +0300

    final refactoring project

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -12,6 +12,6 @@ def generate_round():
     number = randint(1, 25)
     game_question = f'Question: {number}'
 
-    right_answer = 'yes' if is_even(number) is True else 'no'
+    right_answer = 'yes' if is_even(number) else 'no'
 
     return right_answer, game_question
--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,16 +4,21 @@ from random import randint
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
+def is_gcd(first_int, second_int, number):
+    while number != 0:
+        if first_int % number == 0 and second_int % number == 0:
+            return number
+            break
+        else:
+            number -= 1
+
+
 def generate_round():
     first_int = randint(1, 10)
     second_int = randint(1, 10)
     game_question = (f'Question: {first_int} {second_int}')
+
     number = min([first_int, second_int])
-    right_answer = number
-    while number != 0:
-        if first_int % number == 0 and second_int % number == 0:
-            right_answer = number
-            break
-        else:
-            number -= 1
+
+    right_answer = is_gcd(first_int, second_int, number)
     return str(right_answer), game_question
